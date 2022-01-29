a54b970edd45e2fe8424aff4d32a6b7aa56b3ce2
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-29 22:09:07 +0300

    display current date

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from . import main
 from ..request import get_sources
+from datetime import datetime
 
 @main.route('/')
 def index():
   news_sources=get_sources()
   print(news_sources)
-  return render_template('index.html',news_sources=news_sources)
+  time=datetime.now()
+  date_today=time.strftime("%b %d, %Y")
+  return render_template('index.html',news_sources=news_sources,date_today=date_today)
 
