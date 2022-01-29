7769943d72f48c8967f69ca2d36d1862950ad34d
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-29 22:13:57 +0300

    add article class under models

--- app/models.py
+++ app/models.py
@@ -13,3 +13,18 @@ class Source:
 
   def __str__(self):
     return f"{self.id} {self.name}"
+
+class Article:
+  '''
+  Article class that defines news article objects
+  '''
+  def __init__(self, source_name,author,title,description,url,image_url,publication_date,content):
+    self.source_name=source_name
+    self.author=author
+    self.title=title
+    self.description=description
+    self.url=url
+    self.image_url=image_url
+    self.publication_date=publication_date
+    self.content=content
+    
