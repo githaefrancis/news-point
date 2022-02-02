2650db31c3daf22b0cfc9c23dd326cdea38c989e
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-02 10:19:24 +0300

    run test cases

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@ from app import create_app
 
 app=create_app('development')
 
-
+@app.cli.command()
+def test():
+  """
+  Run the unit tests
+  """
+  import unittest
+  tests=unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__=='__main__':
   app.run()
--- tests/test_article.py
+++ tests/test_article.py
@@ -4,7 +4,7 @@ from unittest.main import main
 # app=create_app('development')
 from app.models import Article
 
-class SourceTest(unittest.TestCase):
+class ArticleTest(unittest.TestCase):
   '''
   Test Class to test the behaviour of the Source 
   '''
@@ -13,7 +13,7 @@ class SourceTest(unittest.TestCase):
     '''
     setUp method that will run 
     '''
-    self.new_source=Article('Bbc','Kevin',"sdafsd","rewrfefde","https://sdfds.com","https://sdfds.com","29/01/2022","fdsfdsfdsfdsfdsf")
+    self.new_source=Article('bbc','Bbc','Kevin',"sdafsd","rewrfefde","https://sdfds.com","https://sdfds.com","29/01/2022","fdsfdsfdsfdsfdsf")
 
   def test_instance(self):
     self.assertTrue(isinstance(self.new_source,Article))
